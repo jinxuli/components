21540ee15cb2f30c366196334e40ead1e7edcebc
Air <412682824@qq.com>
2024-12-21 14:07:49 +0800

    feat: 新增组件

---
+++ x-flow/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <div>
+            <span class="box">sdfsfsdf</span>
+        </div>
+        {{ state }}
+        <el-button @click="changeName">点击</el-button>
+        {{ test }}
+    </div>
+</template>
+<script setup>
+import test from '@assets/vue.svg';
+defineOptions({
+    name: 'XFlow',
+});
+
+const state = ref('');
+
+const changeName = (a, b) => {
+    state.value = 'XFlow updated';
+};
+onMounted(() => {
+    console.log('');
+    console.log('sdffdssd');
+});
+</script>
+<style scoped lang="less">
+.box {
+    background-color: @primary;
+}
+</style>
